048fd02793f5aec1f479b28041044d1526035b45
Ninny <45994151+mpia3@users.noreply.github.com>
2022-11-02 11:48:34 +0100

    Delete run_preprocessing_contrast_mining.sh

--- run_preprocessing_contrast_mining.sh
+++
@@ -1,5 +0,0 @@
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-# Contrast pattern mining preprocessing.
-printf "${RED}Preprocessing dataset into ASP trasnsactions facts...${NC}\n"
-python submodules/ASP_preprocessing_contrast_mining.py -x data/"Karen Cook McNally Final.xlsx" -o output_files/Karen/Karen_transactions.lp -c type
